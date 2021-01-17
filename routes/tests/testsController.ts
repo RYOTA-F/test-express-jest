f72234e27c71d0152b91e61f3d7b9a0529610eb2
Ryota Fujishima <r.fujishima@comnico.jp>
2021-01-17 19:32:09 +0900

    first commit

---
+++ routes/tests/testsController.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import { GetTests } from './get_tests'
+
+const router = Router()
+
+router.get('/', (req, res, next) => {
+  new GetTests(req, res).main().catch(next)
+})
+
+export default router
